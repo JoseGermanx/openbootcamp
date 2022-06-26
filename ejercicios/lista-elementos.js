cbbef0a7267114d310147fa9d2d8604d0680f40d
Jose G Martinez <jgermanmm@gmail.com>
2022-06-26 16:38:27 -0400

    ejercicio lista

---
+++ ejercicios/lista-elementos.js
@@ -0,0 +1,10 @@
+let lista = [
+  "Jose G Martinez",
+  40,
+  true,
+  new Date(Date(81, 07, 3)),
+  { titulo: "Arte de la Guerra", 
+  autor: " Sun Tzu", 
+  fecha:1772, 
+  url: "http://www.amazon.com", },
+];
