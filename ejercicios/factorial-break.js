8dcefd2c91f67e145393164e5e32c49102fbb342
Jose G Martinez <jgermanmm@gmail.com>
2022-07-18 20:24:58 -0400

    corrección factorial break

--- ejercicios/factorial-break.js
+++ ejercicios/factorial-break.js
@@ -1,12 +1,12 @@
 function factorial (n) {
 	let resultado = 1;
-    let i = 1; 
     if (n < 0) return "No existe el factorial de un número negativo"
     if (n === 0 || n === 1) return 1
   
-	while (i <= n) {
-		resultado = resultado * i; 
-        i++
+	while (n) {
+		resultado *= n
+		n--
+		if (n === 1) break
 	}
     
 	return resultado; 
